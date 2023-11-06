c8f875559fbbbe682152236e086081e196802385
Shihab Hasan <shihabbd018@gmail.com>
2023-11-07 02:03:58 +0600

    added 404 error page design

--- src/pages/Error/Error.jsx
+++ src/pages/Error/Error.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
 
+import bg from '../../assets/images/error.jpg'
+import { useNavigate } from 'react-router-dom'
+
 const Error = () => {
-    return (
-        <div>Error</div>
-    )
+    const navigate = useNavigate();
+
+
+    return <div className='flex flex-col items-center h-screen justify-center'>
+        <img src={bg} alt="" />
+        <h3 className='font-semibold text-center text-xl text-gray-600 my-5'>The page you are looking for, does not exist.</h3>
+        <button onClick={() => navigate(-1)} className='bg-purple-700 px-5 py-2 rounded-md text-white font-semibold text-lg'>Go Back</button>
+    </div>
 }
 
 export default Error
