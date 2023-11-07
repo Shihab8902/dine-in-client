4fc13fb9213131d5ada820c1243b80f31d7a9585
Shihab Hasan <shihabbd018@gmail.com>
2023-11-08 01:37:04 +0600

    fixes error page redirect issue

--- src/pages/Error/Error.jsx
+++ src/pages/Error/Error.jsx
@@ -10,7 +10,7 @@ const Error = () => {
     return <div className='flex flex-col items-center h-screen justify-center'>
         <img src={bg} alt="" />
         <h3 className='font-semibold text-center text-xl text-gray-600 my-5'>The page you are looking for, does not exist.</h3>
-        <button onClick={() => navigate(-1)} className='bg-purple-700 px-5 py-2 rounded-md text-white font-semibold text-lg'>Go Back</button>
+        <button onClick={() => navigate("/")} className='bg-purple-700 px-5 py-2 rounded-md text-white font-semibold text-lg'>Go Home</button>
     </div>
 }
 
